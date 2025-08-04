2f13b57d69d773656afb1ea67363686827e8a27e
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2025-08-04 09:45:14 -0400

    touchups

--- run_jobs.py
+++ run_jobs.py
@@ -1,10 +1,14 @@
-import os, time
+import os
+import time
 
 group = 0
 
 batch_name = "min_elem_6_constrained_bh_wa_average_difference"
 
-groups_list = [int(csv_file.split(".")[0]) for csv_file in os.listdir(f"data/sweep_configs/{batch_name}")]
+groups_list = [
+    int(csv_file.split(".")[0])
+    for csv_file in os.listdir(f"data/sweep_configs/{batch_name}")
+]
 num_groups = max(groups_list) + 1
 
 user = os.getenv("USER")
@@ -16,16 +20,13 @@ while group < num_groups:
         num_running_or_queued = (
             int(os.popen(f"squeue -u {user} | wc -l").read().strip()) - 1
         )
-        print(
-            f"On batch {group - 1}, Still running {num_running_or_queued} jobs"
-        )
+        print(f"On batch {group - 1}, Still running {num_running_or_queued} jobs")
         time.sleep(600)
 
     print(f"Submitting batch #{group}")
-    os.system(f"sbatch run_batch_discovery.sh {group} {batch_name}")
+    os.system(f"sbatch run_batch.sh {group} {batch_name}")
     time.sleep(3)
     num_running_or_queued = (
         int(os.popen(f"squeue -u {user} | wc -l").read().strip()) - 1
     )
     group += 1
-
