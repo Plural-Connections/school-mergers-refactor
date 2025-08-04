a06628d777abbd1dd52454887a41aa6714bf48df
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2025-08-04 16:51:43 -0400

    restructure dispatch.sh

--- dispatch.sh
+++ dispatch.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 SLURM_MAX_TASKS=1000  # Could be different- use `scontrol show config | grep MaxArraySize`
 
+send_out_batch() {
+    echo Running batch name=${4} ${3}:${1}-${2}
+    sbatch --job-name=${4} --dependency=singleton --array=${1}-${2} run_batch.sh ${3} ${4}
+}
+
 if [[ -z $1 ]]; then
     echo You must pass a batch name. I\'m not smart enough to make my own!
     exit 1
@@ -21,13 +26,13 @@ cat "${batch_dirs[@]}" > $full_file
 lines_left=$(wc -l < $full_file)
 jobs_run=0
 while [[ $lines_left -gt $SLURM_MAX_TASKS ]]; do
-    echo sbatch --job-name=$batchname --dependency=singleton --array=${jobs_run}-$(( $jobs_run + $SLURM_MAX_TASKS )) run_batch.sh $full_file $batchname
+    send_out_batch ${jobs_run} $(( $jobs_run + $SLURM_MAX_TASKS )) $full_file $batchname
     jobs_run=$(( $jobs_run + $SLURM_MAX_TASKS ))
     lines_left=$(( $lines_left - $SLURM_MAX_TASKS ))
 done
 
 if [[ $lines_left -gt 0 ]]; then
-    echo sbatch --job-name=$batchname --dependency=singleton --array=${jobs_run}-$(( $jobs_run + $lines_left )) run_batch.sh $full_file $batchname
+    send_out_batch ${jobs_run} $(( $jobs_run + $lines_left )) $full_file $batchname
 fi
 
 echo Done!
