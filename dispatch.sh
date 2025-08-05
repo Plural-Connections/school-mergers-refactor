b10b4593c0afcdf2479493628ad8334f51b8f6f7
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2025-08-05 17:48:06 -0400

    rework dispatch.sh

--- dispatch.sh
+++ dispatch.sh
@@ -2,10 +2,21 @@
 SLURM_MAX_TASKS=1000  # Could be different- use `scontrol show config | grep MaxArraySize`
 
 send_out_batch() {
-    end=$1
-    shift
-    echo Running batch name=${2} ${1}:0-$end
-    sbatch --job-name=${2} --dependency=singleton --array=0-$end run_batch.sh $jobs_run $@
+    array_end=$1; shift
+    dependency_id=$1; shift
+    if [[ -z $dependency_id ]]; then
+        dependency=""
+    else
+        dependency="--dependency=afterok"
+    fi
+    command=sbatch --job-name=${2} $dependency --array=0-$array_end run_batch.sh $jobs_run $@
+    echo $command
+    sbatch_output=$($command)
+    echo $sbatch_output
+    # sbatch's output looks like: 'Submitted batch job 12345'
+    read _ _ _ job_id <<< $sbatch_output
+    echo Job ID is $job_id
+    echo $job_id >&2
 }
 
 if [[ -z $1 ]]; then
@@ -24,16 +35,26 @@ fi
 full_file=$(mktemp)
 cat "${batch_dirs[@]}" > $full_file
 
+exec 3>&1  # fd 3 redirects to stdout
+
 lines_left=$(wc -l < $full_file)
 jobs_run=0
+all_deps=''
+dependency=''
 while [[ $lines_left -gt $SLURM_MAX_TASKS ]]; do
-    send_out_batch $SLURM_MAX_TASKS $full_file $batchname
+    dependency=$(send_out_batch $SLURM_MAX_TASKS $dependency $full_file $batchname 2>&1 1>&3)
+    all_deps+=afterany:$dependency:
     jobs_run=$(( $jobs_run + $SLURM_MAX_TASKS ))
     lines_left=$(( $lines_left - $SLURM_MAX_TASKS ))
 done
 
 if [[ $lines_left -gt 0 ]]; then
-    send_out_batch $lines_left $full_file $batchname
+    dependency=$(send_out_batch $lines_left $dependency $full_file $batchname 2>&1 1>&3)
+    all_deps+=afterany:$dependency:
 fi
 
-rm $full_file
+exec 3>&-
+
+command=srun --dependency=${dependency: -1} rm $full_file
+echo $command
+$command
