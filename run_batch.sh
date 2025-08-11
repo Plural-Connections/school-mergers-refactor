23b9468ce12cd868863affa590d45526cf625e12
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2025-08-11 15:35:35 -0400

    change flag definitions in run_batch.sh

--- run_batch.sh
+++ run_batch.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-#SBATCH --nodes=1 --ntasks=1
-#SBATCH --time=6:00:00 #Request runtime of 12 hours max
-#SBATCH --ntasks-per-node=1 #Each node does one task
-#SBATCH --cpus-per-task=1 #Each node gets one CPU
-#SBATCH --output logs/output_%j.txt #redirect output to output_JOBID.txt
-#SBATCH --error logs/error_%j.txt #redirect errors to error_JOBID.txt
-#SBATCH --mem=16gb # Memory per processor
-#SBATCH --partition=short #Use the short partition
+#SBATCH --nodes=1 --ntasks=1 --ntasks-per-node=1  # Singleton jobs
+#SBATCH --cpus-per-task=4  # Same as in constants.py
+#SBATCH --time=6:00:00
+#SBATCH --output logs/output_%j.txt --error logs/error_%j.txt
+#SBATCH --mem=16gb
+#SBATCH --partition=short  # Use the short partition
 
 if [[ ( ! -v SLURM_ARRAY_TASK_ID ) || ( -z $2 ) ]]; then
     echo "Usage: sbatch --array=0-<N> run_batch.sh <configs file> <batch name>"
