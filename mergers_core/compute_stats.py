2aa75afa91aaff76f50127ee2b290413a0021e53
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2025-08-16 23:28:31 -0400

    update for name consistency

--- mergers_core/compute_stats.py
+++ mergers_core/compute_stats.py
@@ -2,39 +2,71 @@
 import pandas as pd
 import numpy as np
 
+POPULATION_METRIC = "median_difference"
+
 
 def compute_stats(df):
     dissim_rows = df[
-        (df["dissimilarity_weight"] == 1) & (df["population_consistency_weight"] == 0)
+        (df["dissimilarity_weight"] == 1) & (df["population_metric_weight"] == 0)
     ]
     pop_rows = df[
-        (df["dissimilarity_weight"] == 0) & (df["population_consistency_weight"] == 1)
+        (df["dissimilarity_weight"] == 0) & (df["population_metric_weight"] == 1)
     ]
     both_rows = df[
-        (df["dissimilarity_weight"] == 1) & (df["population_consistency_weight"] == 1)
+        (df["dissimilarity_weight"] == 1) & (df["population_metric_weight"] == 1)
     ]
 
     final_df = pd.DataFrame(
-        np.zeros((3, 4), dtype=np.float64),
+        np.zeros((3, 10), dtype=np.float64),
         columns=[
-            "median %Δdissim",
-            "average %Δdissim",
-            "median %Δpop",
-            "average %Δpop",
+            "med %Δdissim",
+            "avg %Δdissim",
+            "med %Δpop (avg)",
+            "avg %Δpop (avg)",
+            "med %Δpop (med)",
+            "avg %Δpop (med)",
+            "med %Δpop (avgΔ)",
+            "avg %Δpop (avgΔ)",
+            "med %Δpop (medΔ)",
+            "avg %Δpop (medΔ)",
         ],
     )
     for stat_index, row in enumerate([dissim_rows, pop_rows, both_rows]):
         pre_dissim = np.array(row["pre_dissim_bh_wa"])
-        with np.errstate(divide="ignore", invalid="ignore"):
-            Δdissim = (np.array(row["post_dissim_bh_wa"]) - pre_dissim) / pre_dissim
-        pre_pop = np.array(row["pre_population_consistency"])
-        with np.errstate(divide="ignore", invalid="ignore"):
-            Δpop = (np.array(row["post_population_consistency"]) - pre_pop) / pre_pop
+        Δdissim = (np.array(row["post_dissim_bh_wa"]) - pre_dissim) / pre_dissim
+
+        pre_pop_average = np.array(row["pre_population_average"])
+        Δpop_average = (
+            np.array(row["post_population_average"]) - pre_pop_average
+        ) / pre_pop_average
+
+        pre_pop_median = np.array(row["pre_population_median"])
+        Δpop_median = (
+            np.array(row["post_population_median"]) - pre_pop_median
+        ) / pre_pop_median
+
+        pre_pop_average_difference = np.array(row["pre_population_average_difference"])
+        Δpop_average_difference = (
+            np.array(row["post_population_average_difference"])
+            - pre_pop_average_difference
+        ) / pre_pop_average_difference
+
+        pre_pop_median_difference = np.array(row["pre_population_median_difference"])
+        Δpop_median_difference = (
+            np.array(row["post_population_median_difference"])
+            - pre_pop_median_difference
+        ) / pre_pop_median_difference
         final_df.loc[stat_index] = [
-            np.median(Δdissim),
-            np.mean(Δdissim),
-            np.median(Δpop),
-            np.mean(Δpop),
+            np.median(Δdissim) * 100,
+            np.mean(Δdissim) * 100,
+            np.median(Δpop_median) * 100,
+            np.mean(Δpop_median) * 100,
+            np.median(Δpop_average) * 100,
+            np.mean(Δpop_average) * 100,
+            np.median(Δpop_average_difference) * 100,
+            np.mean(Δpop_average_difference) * 100,
+            np.median(Δpop_median_difference) * 100,
+            np.mean(Δpop_median_difference) * 100,
         ]
     final_df.index = ["dissim", "pop", "both"]
     return final_df
