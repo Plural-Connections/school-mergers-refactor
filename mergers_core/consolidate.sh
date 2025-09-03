53c4ccf3aa85a79864fe0e9bd5fbd97516f0514e
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2025-09-03 16:46:38 -0400

    update consolidate.sh
    it was still for the old run

--- mergers_core/consolidate.sh
+++ mergers_core/consolidate.sh
@@ -3,25 +3,16 @@ shopt -s globstar
 # consolidates all the results.csv files found within ../data/results into one large csv file
 # then, deduplicates configurations to ensure that only the most recent run of a configuration is kept
 #
-# the output is called <batchname>.csv where batchname is the only batch in ../data/results or
-# the first argument if there are multiple present
+# the output is called results.csv
 
-pushd ../data/results > /dev/null
-batches=(*)
+olddir=$PWD
+cd ../data/results
 
-if [[ ${#batches[@]} -gt 1 && -z $1 ]]; then
-    echo "multiple batches found: $batches"
-    echo "please specify a batch name as the first argument"
-    exit 1
-fi
-batchname=${1:-${batches[0]}}
-echo "using batch $batchname"
-
-files=($batchname/**/analytics.csv)
+files=(**/analytics.csv)
 files=($(ls -t "${files[@]}"))
 echo "header is from ${files[0]}"
-head -n 1 "${files[0]}" > $batchname.csv  # get csv column names once
-tail -q -n +2 "${files[@]}" | cut -d, -f -40 >> $batchname.csv  # skip csv column names
+head -n 1 "${files[0]}" > results.csv  # get csv column names once
+tail -q -n +2 "${files[@]}" | cut -d, -f -29 >> results.csv  # skip csv column names
 
-popd > /dev/null
-# python3 consolidate_deduplicate.py $batchname
+python3 $olddir/consolidate_deduplicate.py
+cd $olddir
