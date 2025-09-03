851c274714636e937a7b0b53fc71a956dd20f086
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2025-09-03 16:46:11 -0400

    remove unnecessary prints and deduplicating columns from consolidate_deduplicate.py

--- mergers_core/consolidate_deduplicate.py
+++ mergers_core/consolidate_deduplicate.py
@@ -1,31 +1,28 @@
 # Run by consolidate.sh
 
 import pandas as pd
-import sys
 
 
-batchname = sys.argv[1]
-df = pd.read_csv(f"{batchname}.csv", dtype={"district": str})
+df = pd.read_csv("results.csv", dtype={"district": str})
 columns_to_deduplicate = [
     "district",
-    "school_decrease_threshold",
+    # "school_decrease_threshold",
     "dissimilarity_weight",
     "population_metric_weight",
-    "population_metric",
-    "dissimilarity_flavor",
+    # "population_metric",
+    # "dissimilarity_flavor",
 ]
-print(df)
-print(df[columns_to_deduplicate])
 
 duplicated_mask = df.duplicated(subset=columns_to_deduplicate, keep="first")
-print(duplicated_mask)
 dropped_count = duplicated_mask.sum()
 
 if dropped_count > 0:
-    print(f"dropped {dropped_count} line(s):")
+    print(f"dropped {dropped_count} duplicates(s):")
     dropped_keys = df[duplicated_mask][columns_to_deduplicate].drop_duplicates()
     print("Dropped keys:")
     print(dropped_keys)
+else:
+    print("no duplicates")
 
 df = df[~duplicated_mask]
-df.to_csv(f"{batchname}.csv", index=False)
+df.to_csv("results.csv", index=False)
