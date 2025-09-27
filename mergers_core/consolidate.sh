983f79466736c7d9f572939b077f5ba5abfdd94b
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2025-09-27 17:38:02 -0400

    that's more common than you think
    that code has been there forever i just didn't want to document it lmao

--- mergers_core/consolidate.sh
+++ mergers_core/consolidate.sh
@@ -12,7 +12,7 @@ files=(**/analytics.csv)
 files=($(ls -t "${files[@]}"))
 echo "header is from ${files[0]}"
 head -n 1 "${files[0]}" > results.csv  # get csv column names once
-tail -q -n +2 "${files[@]}" | cut -d, -f -29 >> results.csv  # skip csv column names
+tail -q -n +2 "${files[@]}" | cut -d, -f -29 >> results.csv  # skip csv column names and possible extra columns
 
 python3 $olddir/consolidate_deduplicate.py
 cd $olddir
